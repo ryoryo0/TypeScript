5bbdd65e329f2fc8ae2e39473a1c78e92892adfc
ryosukeharada <web.harada1111@gmail.com>
2024-07-26 07:10:59 +0900

    タイプガード、型キャストなどの学習

--- app.ts
+++ app.ts
@@ -1,19 +1,124 @@
-interface Person {
+type Admin = {
     name: string;
-    age : number;
-
-    greet(phrase: string): void;
+    privileges: string[];
 }
 
-let user1: Person;
+type Employeee = {
+    name: string;
+    startDate: Date;
+}
 
-user1 = {
+type ElevatedEmploee = Admin & Employeee;
+
+const e1: ElevatedEmploee = {
     name: '原田',
-    age : 26,
+    privileges: ['原田'],
+    startDate: new Date(),
+}
 
-    greet(phrase: string) {
-        console.log(phrase + '' + this.name);
+type Combinable1 = string | number;
+type Combinable2 = Date | number;
+
+type Combinable3 = Combinable1 & Combinable2;
+
+type newEmployeee = Admin | Employeee;
+
+function unknowEmployee(emp: newEmployeee) {
+    if ('privileges' in emp) [
+        console.log(emp.privileges)
+    ]
+    
+    if ('startDate' in emp) [
+        console.log(emp.startDate)
+    ]
+}
+
+unknowEmployee(e1);
+
+// 型ガードに関する練習
+//　車について
+
+class Car {
+    drive() {
+        console.log('運転中...');
     }
 }
 
-user1.greet('こんにちは！');
+class Track {
+    private amount: number;
+
+    constructor(amount: number) {
+        this.amount = amount;
+    }
+
+    drive() {
+        console.log('トラックを運転中...');
+    }
+
+    loadCargo() {
+        console.log(`荷物の重量は${this.amount}kgです`);
+    }
+}
+
+type Vehicle = Car | Track;
+
+let v1 = new Car();
+let v2 = new Track(200);
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo();
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+
+interface Bird {
+    type: 'bird',
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse',
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal (animal: Animal) {
+    let speed;
+
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+
+    console.log('移動速度' + speed );
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10});
+
+// 型キャストについて
+let userInput = document.querySelector('input');
+
+if (userInput) {
+    (userInput as HTMLInputElement).value = 'こんにちは';
+}
+
+// インデックス型について
+interface ErrorContainer {
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: '正しいメール￥アドレスではありません',
+    userName: 'ユーザー名に記号を含める事はできません',
+};
